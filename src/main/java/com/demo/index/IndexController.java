cd70f7a08b4ea273984f8d6094ee0a345d041302
Jason_Zhao <zhaoshiying6688@163.com>
2017-10-25 20:54:38 +0800

    spaces replace tab

--- src/main/java/com/demo/index/IndexController.java
+++ src/main/java/com/demo/index/IndexController.java
@@ -5,13 +5,13 @@ import com.jfinal.core.Controller;
 /**
  * 本 demo 仅表达最为粗浅的 jfinal 用法，更为有价值的实用的企业级用法
  * 详见 JFinal 俱乐部: http://jfinal.com/club
- * 
+ * <p>
  * IndexController
  */
 public class IndexController extends Controller {
-	public void index() {
-		render("index.html");
-	}
+    public void index() {
+        render("index.html");
+    }
 }
 
 
